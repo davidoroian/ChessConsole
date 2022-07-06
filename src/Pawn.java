6c70cd41513a18bce9bc7ad8b813cee625752c53
davidoroian <oroiandavid21@gmail.com>
2022-07-06 19:02:54 +0300

    Pawn movePossible method update

--- src/Pawn.java
+++ src/Pawn.java
@@ -16,18 +16,30 @@ public class Pawn extends ChessPiece {
 
     @Override
     public boolean movePossible(int[][] board, int x, int y) {
-        if (((this.getX()==1&&this.id==7)||(this.getX()==6&&this.id==1))&& Math.abs(this.getX()-x)==2 && (this.getX() != x || this.getY() != y))
-            if (y == this.getY() && board[x][y] == 0) { //forward if no piece in front
-                if (board[x][y] == 0) return true;
+        switch (color) {
+            case "black" -> {
+                if (this.getX() == 6 && this.getX()-x==2) { //if at the starting position, it can move 2 positions in front
+                    if (y == this.getY() && board[x][y] == 0 && board[x+1][y] == 0) return true;
+                }
+                if ((x == this.getX() - 1 && x >= 0 && x < 8) && (y >= 0 && y < 8)) { //one step forward at a time in any direction
+                    if (y == this.getY() && board[x][y] == 0)  return true;//forward if no piece in front
+                    if ((y == this.getY() - 1 || y == this.getY() + 1) && board[x][y] != 0) { //to the sides to attack
+                        if (board[x][y] >= 7 && board[x][y] <= 12) return true;
+                    }
+                }
             }
-        if (Math.abs(this.getX()-x)==1 && (x >= 0 && x < 8) && (y >= 0 && y < 8) && (this.getX() != x || this.getY() != y)) { //one step forward at a time in any direction
-            if (y == this.getY() && board[x][y] == 0) { //forward if no piece in front
-                if (board[x][y] == 0) return true;
+            case "white" -> { //same but for white
+                if (this.getX() == 1 && this.getX()-x==-2) { //if at the starting position, it can move 2 positions in front
+                    if (y == this.getY() && board[x][y] == 0 && board[x-1][y] == 0) return true;
+                }
+                if ((x == this.getX() + 1 && x >= 0 && x < 8) && (y >= 0 && y < 8)) {
+                    if (y == this.getY() && board[x][y] == 0) return true;
+                    if ((y == this.getY() - 1 || y == this.getY() + 1) && board[x][y] != 0) {
+                        if (board[x][y] >= 1 && board[x][y] <= 6) return true;
+                    }
+                }
             }
-            if (Math.abs(this.getY()-y)==1 && board[x][y] != 0) { //to the sides to attack
-                if (board[x][y] >= 7 && board[x][y] <= 12 && this.id==1) return true;
-                if (board[x][y] >= 1 && board[x][y] <= 6 && this.id==7) return true;
-            }
+            default -> throw new IllegalStateException("Unexpected value: " + color);
         }
         return false;
     }
